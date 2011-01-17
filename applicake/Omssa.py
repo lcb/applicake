8ca0bb8026c3d14172bc3b678326b26b89ee9a87
quandt.andreas <quandt.andreas@a697f730-0300-b9c8-cee0-f846692decd2>
2011-01-17 23:21:12 +0000

    - finished omssa.py (included dynamic setting of -teppm which is available from version 2.1.9 on)
    git-svn-id: http://applicake.googlecode.com/svn/trunk@18 a697f730-0300-b9c8-cee0-f846692decd2

--- applicake/Omssa.py
+++ applicake/Omssa.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 '''
 Created on Jan 17, 2011
 
@@ -6,34 +7,42 @@ Created on Jan 17, 2011
 
 import sys,os
 import shutil
+from string import Template
 from applicake.app import TemplateApplication
+from applicake.utils import Utilities
 
 class Omssa(TemplateApplication):
     '''
     classdocs
     '''
     def _get_app_inputfilename(self,config):
-        src = self._template_filename
-        dest = os.path.join(a._wd,'omssa.params')
-        shutil.move(src, dest)
-        return dest
+        dest = os.path.join(self._wd,'omssa' + self._params_ext)
+        Utilities().substitute_template(template_filename=self._template_filename,dictionary=config,output_filename=dest)
+        return dest        
     
     def _get_command(self,prefix,input_filename):
-        search_filename = self._iniFile.read_ini()['SEARCH']
-        
-        new_mzxml = os.path.join(self._wd,os.path.split(mzxml_filename)[1])
-        params = open(input_filename,'r').read()
+        config = self._iniFile.read_ini()
+        search_filename = config['SEARCH']
+        content = open(input_filename,'r').read()
+        params = Template(content).safe_substitute(config)
         self.log.debug('parameter [%s]' % params)
-        type = params.split(' ')[0][1:]
-        self.log.debug('search file type [%s]' % type)
-        basename = os.path.basename(mzxml_filename)
-        root= os.path.splitext(basename)[0]        
-        self.result_filename  = os.path.join(self._wd,root + "." + type)
-        self._iniFile.add_to_ini({'SEARCH':self.result_filename})
-        return "ln -s %s %s;%s %s %s" %(mzxml_filename,new_mzxml,prefix,params,new_mzxml)
+        if config['PRECMASSUNIT'].lower() is "ppm":
+            params = params + ' -teppm'
+            self.log.debug('added [ -teppm] to parameters')       
+        self._result_filename  = os.path.join(self._wd,self.name + ".pepxml")
+        self._iniFile.add_to_ini({'PEPXML':self._result_filename})
+#        ' -fm '+input_path+' -op '+ output_path+
+        return "%s %s -fm %s -op %s" %(prefix,params,search_filename,self._result_filename)
     
     def _validate_run(self,run_code):               
-        return super(Omssa, self)._validate_run(run_code)
+        exit_code = super(Omssa, self)._validate_run(run_code)
+        if 0 != exit_code:
+            return exit_code
+        for line in self.stderr.readlines():
+            if not line.startswith('Info:'):
+                self.log.error("stderr contains following line [%s]" % line)
+                return 1
+        return 0    
         
 if "__main__" == __name__:
     # init the application object (__init__)
