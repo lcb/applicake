7ff1d0a069a21b380056d410520182d8a2480279
quandt.andreas <quandt.andreas@a697f730-0300-b9c8-cee0-f846692decd2>
2011-05-12 14:21:19 +0000

    - add onliner.py: provides functionality for execution of small command lines via -p argument -- e.g.: -p 'cd ~;ls -lhart'
    git-svn-id: http://applicake.googlecode.com/svn/trunk@86 a697f730-0300-b9c8-cee0-f846692decd2

---
+++ applicake/oneliner.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+'''
+Created on May 12, 2011
+
+@author: quandtan
+'''
+
+
+import sys,os,shutil
+from applicake.app import ExternalApplication as app
+
+# init the application object (__init__)
+a = app(use_filesystem=False,name=None)
+exit_code = a(sys.argv)
+#copy the log file to the working dir
+#for filename in [a._log_filename,a._stderr_filename,a._stdout_filename]:
+#    shutil.move(filename, os.path.join(a._wd,filename))
+print(exit_code)
+sys.exit(exit_code)
