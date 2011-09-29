a5d975443a6f0e2ff546072b929553adac1f58b5
quandt.andreas <quandt.andreas@a697f730-0300-b9c8-cee0-f846692decd2>
2011-09-29 07:22:25 +0000

    - wrote an optimized fdr2probability.py based on tabular - made 'backup' of old version in fdr2probabilityDB.py
    git-svn-id: http://applicake.googlecode.com/svn/trunk@94 a697f730-0300-b9c8-cee0-f846692decd2

--- applicake/fdr2probability.py
+++ applicake/fdr2probability.py
@@ -5,7 +5,9 @@ Created on Feb 21, 2011
 @author: quandtan
 '''
 #
-import argparse,os,sys,fileinput,logging,glob,sqlite3,csv
+import argparse,os,sys,fileinput,logging,glob,csv
+import tabular as tb
+import time
 from applicake.app import Application
         #
 class Fdr2Probability(Application):
@@ -42,98 +44,108 @@ class Fdr2Probability(Application):
     def _get_probability(self,fdr_col,prob_col):
         cutoff_limit = 0.001
         num_limit = 100
-        prob = None        
-        self._sql.execute('select count(%s) from %s where %s < %s' % (prob_col,self._tbl_name,fdr_col,self._cutoff) )
-        num = self._sql.fetchone()[0]
+        prob = None       
+        data_cutoff = self._data[self._data[fdr_col]<=self._cutoff]
+        num = len(data_cutoff)
         if num < num_limit :
-            self.log.error('number of PSMs [%s] matching the probability cutoff [%s][%s] is below the threshold of [%s]' % (num,prob_col,self._cutoff,num))
+            self.log.error('number of PSMs [%s] matching the probability cutoff [%s][%s] is below the threshold of [%s]' % (num,prob_col,self._cutoff,num_limit))
             sys.exit(1)  
         else:
-            self.log.debug('number of PSMs [%s] matching the probability cutoff [%s][%s]' % (num,prob_col,self._cutoff))                           
-        try:
-            self._sql.execute('select %s from %s where %s < %s order by %s desc limit 1 ' % (prob_col,self._tbl_name,fdr_col,self._cutoff,fdr_col) )            
-            prob = self._sql.fetchone()[0]
-        except:
-            self.log.error('could not fetch a probability value.')
-            sys.exit(1)
+            self.log.debug('number of PSMs [%s] matching the probability [%s] with FDR cutoff [%s]' % (num,prob_col,self._cutoff))
+        # need to sort by fdr_col and prob_col. otherwise the num of peps differ for the the fdr-cutoff and the prob-cutoff 
+        data_cutoff.sort(order=[prob_col])
+        prob = data_cutoff[prob_col][0]
         if prob < cutoff_limit: 
-            self.log.debug('probabiliity [%s] is below the cutoff value [%s]. therefore cutoff value is returned.' % (prob,cutoff_limit))
-            prob = cutoff_limit
+            self.log.debug('probability [%s] is below the cutoff limit [%s]. therefore cutoff limit is applied.' % (prob,cutoff_limit))
+            prob = cutoff_limit        
+        else:
+            self.log.debug('probability [%s] matches FDR [%s].' % (prob,data_cutoff[fdr_col][0]))        
+        self.log.debug('num of peptides >= probability[%s] [%s]' % (prob,len(data_cutoff[data_cutoff[prob_col]>=prob]))) 
         return prob               
     #
     def _preprocessing(self):
         self.log.debug('read [%s]' % self._input_filename)
-        reader = csv.DictReader(open(self._input_filename), delimiter='\t')
-        header = reader.fieldnames
-        new_col_names = ['FDR_PPROPHET','FDR_IPROPHET']      
-        self.log.debug('init db')
-        tbl_name = 'pepcsv'
-        con = sqlite3.connect(':memory:') 
-#        con = sqlite3.connect('fdr2probability.db') 
-        # If you want autocommit mode, then set isolation_level to None
-        con.isolation_level = None
-        sql = con.cursor()
-        cols =["%s int primary key"% header[0]]
-        cols +=["%s varchar(100) not null"% header[1]]
-        cols +=["%s double not null"% header[2]]
-        cols +=["%s int(11) not null"% header[3]]
-        cols +=["%s varchar(50) not null"% header[4]]
-        cols +=["%s varchar(100) not null"% header[5]]
-        cols +=["%s varchar(255) not null"% header[6]]
-        cols +=["%s char(1) not null"% header[7]]
-        cols +=["%s char(1) not null"% header[8]]
-        cols +=["%s varchar(150) not null"% header[9]]
-        cols +=["%s double not null"% header[10]]
-        cols +=["%s double not null"% header[11]]
-        cols +=["%s int(11) not null"% header[12]]
-        cols +=["%s double not null"% header[13]]
-        cols +=["%s double not null"% header[14]]   
-        cols +=["%s double"% new_col_names[0]]
-        cols +=["%s double"% new_col_names[1]]         
-        sql.execute("drop table if exists %s" % tbl_name)
-        sql.execute('create table if not exists %s (%s)' % (tbl_name,','.join(cols)))            
-        for i in reader:
-            keys = '"' + '","'.join(i.keys()) + '"'
-            values = '"' + '","'.join(i.values()) + '"'                                    
-            cmd  = 'insert into %s (%s) values (%s)' % (tbl_name,keys,values)
-            sql.execute(cmd)
-        self._con = con
-        self._tbl_name = tbl_name
-        self._sql = self._con.cursor()       
+        self._data = tb.tabarray(SVfile=self._input_filename)
+        peptides = self._data['peptide'].tolist()
+        self.log.debug('num of peptides [%s]' % len(peptides))      
+        uniq_peptides = list(set(peptides)) 
+        self.log.debug('num of unique peptide sequences [%s]' % len(uniq_peptides))        
         #
     def _calc_fdr_psm(self, dict):
-        self._probability_cutoffs = {}
-        for k in dict.keys(): 
-            sql = self._con.cursor()
-            sql2 = self._con.cursor()
-            sql.execute('select protein,nrow from %s order by %s desc' % (self._tbl_name,dict[k]))
+        for k in dict.keys():
+            self._data.sort(order=[dict[k]])
+            proteins = self._data['protein']
+            fdr_vals = []
             t = 0
             f = 0  
             fdr_val = None      
-            for row in sql:
-                if self._decoy in row[0]: f +=1
+            # data have to be reversed because the previous sorting is low -> high
+            # we need however high ->
+            for e in proteins[::-1]:
+                if self._decoy in e: f +=1
                 else: t +=1
                 fdr_val = float(f) / (float(t) + float(f))
-                sql2.execute('update %s set %s=%s where nrow=%s' % (self._tbl_name,k,fdr_val,row[1]))
-                               
+                fdr_vals.append(fdr_val)
+            # add re-reversed values that the order matches again what is in the data object
+            self._data = self._data.addcols([fdr_vals[::-1]],names=[k]) 
+            self.log.debug('finished calculating fdr for [%s]' % k)                              
                 
-    def _cal_fdr_peptide(self,dict):
-        for k in dict.keys(): 
-            sql = self._con.cursor()
-            sql2 = self._con.cursor()
-            sql.execute('select distinct peptide from %s ' % self._tbl_name)
-            uniq_peps = [row[0] for row in sql]
-            sql.execute('select protein,nrow,peptide from %s order by %s desc' % (self._tbl_name,dict[k]))
+    def _cal_fdr_peptide(self,dict):            
+        for k in dict.keys():      
+            self._data.sort(order=[dict[k]])
+#            data = self._data[['protein','peptide']]
+            proteins = self._data['protein']
+            proteins = proteins[::-1]
+            peptides = self._data['peptide']
+            peptides = peptides[::-1]
+            mod_peptides = self._data['modified_peptide']
+            mod_peptides = mod_peptides[::-1]
+            
+            peptides_nomods_noD = [e['peptide'] for e in self._data[['protein','peptide']] if 'DECOY_' not in e['protein']]
+            peptides_mods_noD = [e['modified_peptide'] for e in self._data[['protein','modified_peptide']] if 'DECOY_' not in e['protein']] 
+            all_peptides_noD = reduce(lambda x, y: x+y, [peptides_nomods_noD + peptides_mods_noD])
+            print len(all_peptides_noD)
+            uniq_peptides_noD = list(set(all_peptides_noD))
+            uniq_peptides_noD.remove('NA')
+            print len(uniq_peptides_noD)
+            
+            found_pep_ions = []
+            print 'finished data'
+            fdr_vals = []
             t = 0
             f = 0  
             fdr_val = None      
-            for row in sql:
-                if row[2] in uniq_peps:
-                    uniq_peps.remove(row[2])
-                    if self._decoy in row[0]: f +=1
-                    else: t +=1
+            
+            print 'finished uniq_peptides' 
+            # data have to be reversed because the previous sorting is low -> high
+            # we need however high ->
+            counter = 0
+            chunk = 1000
+            tic = time.clock()
+            for i,protein in enumerate(proteins):
+                if self._decoy in protein: 
+                    f +=1
                     fdr_val = float(f) / (float(t) + float(f))
-                sql2.execute('update %s set %s=%s where nrow=%s' % (self._tbl_name,k,fdr_val,row[1]))   
+                else:
+                    ion = None
+                    if mod_peptides[i] == 'NA':
+                        ion = peptides[i]
+                    else:
+                        ion = mod_peptides[i]
+                    if ion not in  found_pep_ions:
+                        found_pep_ions.append(ion)               
+                        t +=1
+                        fdr_val = float(f) / (float(t) + float(f))                   
+                fdr_vals.append(fdr_val)
+                counter +=1
+                if counter == chunk:
+                    chunk += counter
+                    print counter
+                    print time.clock() - tic     
+            # add re-reversed values that the order matches again what is in the data object
+            self._data = self._data.addcols([fdr_vals[::-1]],names=[k]) 
+            print len(found_pep_ions)
+            self.log.debug('finished calculating fdr for [%s]' % k)             
             #                                    
     def _run(self,command=None):        
         dict = {'FDR_PPROPHET':'probability_pp','FDR_IPROPHET':'probability_ip'}
@@ -141,25 +153,11 @@ class Fdr2Probability(Application):
         if self._prophet == 'pp': idx = 0
         else: idx =1 
         if self._level == 'psm':
-#            key = dict.keys()[idx]
-#            dict1 = {key:dict[key]}
             self._calc_fdr_psm(dict)
         else:
-#            key = dict.keys()[idx]
-#            dict = {key:dict[key]}
-            self._cal_fdr_peptide(dict)         
-        self._write_tsv(dict.keys()[idx])                     
+            self._cal_fdr_peptide(dict)  
+#        self._data.saveSV(self._output_filename,delimiter=self.sep)                     
         print self._get_probability(dict.keys()[idx],dict.values()[idx])
-        self._sql.execute('select count(distinct peptide) from %s ' % self._tbl_name)
-        self.log.debug('num of uniq peptides [%s]' % self._sql.fetchone()[0])
-        self._sql.execute('select count(peptide) from %s ' % self._tbl_name)
-        self.log.debug('num of  peptides [%s]' % self._sql.fetchone()[0])
-        self._sql.execute('select count(distinct peptide) from %s where %s < %s' % (self._tbl_name,dict.keys()[idx],self._cutoff))
-        self.log.debug('num of uniq peptides with cutoff [%s]' % self._sql.fetchone()[0])
-        self._sql.execute('select count(peptide) from %s where %s < %s' % (self._tbl_name,dict.keys()[idx],self._cutoff))
-        self.log.debug('num of  peptides with cutoff [%s]' % self._sql.fetchone()[0])
-        
-
         #
     def _validate_parsed_args(self,dict):           
         self._input_filename = dict['input_filename']
@@ -186,18 +184,7 @@ class Fdr2Probability(Application):
             sys.exit(1)                 
     #       
     def _validate_run(self,run_code=None):                   
-        return 0
-    #
-    def _write_tsv(self,col_sort):
-        fh = open(self._output_filename, 'w')
-        query = 'select * from %s order by %s' % (self._tbl_name, col_sort)
-        self.log.debug(query)
-        self._sql.execute(query)
-        col_name_list = [tuple[0] for tuple in self._sql.description]
-        fh.write("\t".join(col_name_list) + "\n")
-        for row in self._sql:
-            line = '\t'.join([str(i) for i in row])
-            fh.write(line + "\n")    
+        return 0  
     #   
 if '__main__'==__name__:       
     a = Fdr2Probability(use_filesystem=False)
