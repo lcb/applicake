d5956335fd6b534c3284eb65776b41c7c9082e8b
quandt.andreas <quandt.andreas@a697f730-0300-b9c8-cee0-f846692decd2>
2011-11-29 21:02:42 +0000

    - bowtie: work in progress
    git-svn-id: http://applicake.googlecode.com/svn/trunk@131 a697f730-0300-b9c8-cee0-f846692decd2

--- applicake/bowtie.py
+++ applicake/bowtie.py
@@ -13,16 +13,18 @@ class Bowtie(WorkflowApplication):
     def _get_command(self, prefix, input_filename):
         config = self._iniFile.read_ini()
         bowtiebuild = config['BOWTIEBUILD']
-        dir = config['DSSOUTPUT']
-        fastqs = glob.glob('%s/*.fastq' % dir)
-        if not len(fastqs) == 1:
-            self.log.error('found less or more that 1 fastq file [%s] in the dss output dir [%s].' % (fastqs,dir))
-            sys.exit(1)             
+        fastq = config['DSSOUTPUT']
+#        dir = config['DSSOUTPUT']
+#        fastqs = glob.glob('%s/*.fastq' % dir)
+#        if not len(fastqs) == 1:
+#            self.log.error('found less or more that 1 fastq file [%s] in the dss output dir [%s].' % (fastqs,dir))
+#            sys.exit(1)
+#        fastq = fastqs[0]             
         out_fname =  '%s%s' % (self.name,self._result_ext)  
         self._result_filename  = os.path.join(self._wd, out_fname)
         self._iniFile.add_to_ini({'SAM': self._result_filename})
         self._iniFile.add_to_ini({'EXPORT2OPENBIS': self._result_filename})
-        return "%s -S -p %s %s -s %s -c -q %s %s" % (prefix,'2', bowtiebuild,fastqs[0],fastq[0], self._result_filename)
+        return "%s -S -p %s %s -s %s -c -q %s %s" % (prefix,'2', bowtiebuild,fastq,fastq, self._result_filename)
     #bowtie -S -p 2 bowtie_out -s in.fastq -c -q in.fastq out.sam
     
     def _validate_run(self, run_code):
