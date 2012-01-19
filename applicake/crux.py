bf10b10f751b6dbd9b6aa2d6bfaf229d6b61920c
quandt.andreas <quandt.andreas@a697f730-0300-b9c8-cee0-f846692decd2>
2012-01-19 21:01:01 +0000

    - crux.py: moved validate_run() from cruxsearch.py - changed checks in validate_run()
    git-svn-id: http://applicake.googlecode.com/svn/trunk@136 a697f730-0300-b9c8-cee0-f846692decd2

--- applicake/crux.py
+++ applicake/crux.py
@@ -33,11 +33,30 @@ class Crux(TemplateApplication):
         config = self._iniFile.read_ini()
         search_filename = config['MZXML']
         db_filename = config['DBASE'] 
-        db_filename = db_filename.replace('.fasta','.fasta.idx')   
+        db_filename = db_filename + '.idx'   
         self._result_filename  = os.path.join(self._wd,self.name + self.get_pepxml_suffix())
         self._iniFile.add_to_ini({'PEPXML':self._result_filename})
         # crux search-for-matches --parameter-file my.params --fileroot B08-02057 B08-02057.mzXML --output-dir . /cluster/scratch/malars/databases/AE004092_sp_9606.idx
         return "%s %s --parameter-file %s --fileroot %s %s --output-dir %s %s" %(prefix,self.get_crux_command(),input_filename,self.name,search_filename,self._wd,db_filename)
     
-    def _validate_run(self,run_code):                       
-        raise NotImplementedError("Called '_validate' method on abstact class")   
+#    def _validate_run(self,run_code):                       
+#        raise NotImplementedError("Called '_validate' method on abstact class") 
+#    
+    def _validate_run(self,run_code):               
+        exit_code = super(Crux, self)._validate_run(run_code)
+        if 0 != exit_code:
+            return exit_code
+        return 0
+#        stderr = self.stderr.read()
+#        if 'INFO: Return Code:0' in stderr:
+#            self.log.debug('crux finished %s' % self.get_crux_command())
+#            return 0
+#        else:         
+##        for line in self.stderr.read():
+##            if line.startswith('INFO: Return Code:0'):
+##                self.log.debug('crux finished %s' % self.get_crux_command())
+##                return 0                
+#            self.log.error("crux did not finish %s properly" % self.get_crux_command())
+#            self.log.debug(stderr)
+#            self.log.debug(self.stderr.read())
+#            return 1       
--- applicake/cruxsearch.py
+++ applicake/cruxsearch.py
@@ -17,16 +17,16 @@ class CruxSearch(Crux):
     def get_pepxml_suffix(self):
         return ".search.target.pep.xml"  
     
-    def _validate_run(self,run_code):               
-        exit_code = super(Crux, self)._validate_run(run_code)
-        if 0 != exit_code:
-            return exit_code
-        for line in self.stderr.readlines():
-            if line.startswith('INFO: Finished crux-search-for-matches'):
-                self.log.debug('crux finished %s' % self.get_crux_command())
-                return 0                
-        self.log.error("crux did not finish %s properly" % self.get_crux_command())
-        return 1          
+#    def _validate_run(self,run_code):               
+#        exit_code = super(Crux, self)._validate_run(run_code)
+#        if 0 != exit_code:
+#            return exit_code
+#        for line in self.stderr.readlines():
+#            if line.startswith('INFO: Return Code:0:'):
+#                self.log.debug('crux finished %s' % self.get_crux_command())
+#                return 0                
+#        self.log.error("crux did not finish %s properly" % self.get_crux_command())
+#        return 1          
     
 
 if "__main__" == __name__:
