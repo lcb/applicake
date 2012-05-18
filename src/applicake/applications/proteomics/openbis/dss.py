f91381ef5e96280f9b2127e5e0b0f742ea297751
emanuelschmid@yahoo.com <emanuelschmid@yahoo.com@a697f730-0300-b9c8-cee0-f846692decd2>
2012-05-18 09:58:00 +0000

    adaptations for new interface, contains set_args()
    git-svn-id: http://applicake.googlecode.com/svn/trunk@251 a697f730-0300-b9c8-cee0-f846692decd2

--- src/applicake/applications/proteomics/openbis/dss.py
+++ src/applicake/applications/proteomics/openbis/dss.py
@@ -7,13 +7,13 @@ Modified on Mar 6, 2012 by schmide, CHANGE_NAME enabled -> 0.1.2
 Modified on Mar 26, 2012 by quandtan, key naming -> 0.1.3
 Modified on Apr 3, 2012 by schmide, default: change name, stop on failure; key naming parameterized + defaults -> 0.1.4
 Modified on May 10, 2012 by schmide, new applicake version -> 0.2.0
+Modified on May 18, 2012 by schmide, new interface, contains set_args() -> 0.2.1
 @author: quandtan, behullar, schmide
-@version: 0.2.0
+@version: 0.2.1
 '''
 
-import os,sys,shutil
+import os,sys
 from applicake.framework.interfaces import IWrapper
-from applicake.framework.enums import KeyEnum
 
 TRUES = ['TRUE', 'T', 'YES', 'Y', '1']
 class NoDataSetFound(Exception):
@@ -21,80 +21,69 @@ class NoDataSetFound(Exception):
 
 class Dss(IWrapper):
     DEFAULT_KEYS = {'getmsdata':'MZXML', 'getexperiment':'SEARCH'}
-    MANDATORY_KEYS = ['DATASET_CODE', 'DATASET_DIR']
     ALLOWED_PREFIXES = ['getdataset', 'gettransitions', 'getmsdata', 'getexperiment']
     
-#    '''
-#    this is a workaround for the seemingly disfunctional log
-#    '''
-#    def _log(self, cmet, level, msg):
-#        cmet(msg)
-#        print "%s: %s" % (level,msg)
-
     """
     Interface for application that wraps an external application
     """
     def prepare_run(self, info, log):
         """
-        Prepare the execution of an external program.
-        
-        @type info: dict         
-        @param info: Dictionary object with information needed by the class
-        @type log: Logger 
-        @param log: Logger to store log messages 
-        
-        @rtype: (string,dict)
-        @return: Tuple of 2 objects; the command to execute and the (updated) info object.
+        See interface
         """
         log.debug(info)
-        prefix = info[KeyEnum.PREFIX]
+        prefix = info[self.PREFIX]
         info[self.DSSCLIENT] = prefix
         if not prefix in Dss.ALLOWED_PREFIXES:
-            self._log(log.error, 'ERROR', "prefix ('%s') must be one of %s" % (prefix, Dss.ALLOWED_PREFIXES))
+            log.error("prefix ('%s') must be one of %s" % (prefix, Dss.ALLOWED_PREFIXES))
             sys.exit(1)
         self._result_filename = prefix+".out"
         try: os.remove(self._result_filename)
         except: pass
-        for key in Dss.MANDATORY_KEYS:
-            try: info[key]
-            except KeyError:
-                log.error("Missing mandatory property in inifile: %s" % key)
-                sys.exit(2)
-        # output is written to all ini-properties who's keys are listed in in Dss.outkeys.
-        # if the keys are not given as ini property 'DSSKEYS', 
-        # the default is 'DSSOUTPUT' plus an optional, prefix specific key.
-        try: 
-            dss_keys = info[self.DSSKEYS]
-            if type(dss_keys) is str: self.outkeys = [dss_keys]
-            elif type(dss_keys) is list: self.outkeys = dss_keys
-            else: raise Exception('goto except')
-        except:
+        
+        dss_keys = info[self.DSSKEYS]
+        if type(dss_keys) is str:
+            self.outkeys = [key.strip() for key in dss_keys.split(',')]
+        elif type(dss_keys) is list:
+            self.outkeys = dss_keys
+        else:
+            log.error("%s (%s) must be either str or list" % (self.DSSKEYS, self.info[self.DSSKEYS]))
+            sys.exit(2)
+        while True:
+            try: self.outkeys.remove('')
+            except: break
+        if len(self.outkeys) == 0:
             self.outkeys = [self.DSSOUTPUT]
             try: self.outkeys.append(Dss.DEFAULT_KEYS[prefix])
             except: pass
+        
         dataset_codes = info[self.DATASET_CODE]
         if type(dataset_codes) is str:
             self._codes = [code.strip() for code in dataset_codes.split(',')]
         elif type(dataset_codes) is list:
             self._codes = dataset_codes
         else:
-            log.error("%s must be either str or list" % self.DATASET_CODE)
+            log.error("%s (%s) must be either str or list" % (self.DATASET_CODE, self.info[self.DATASET_CODE]))
             sys.exit(3)
+        
         outdir = info[self.DATASET_DIR]
         if not os.path.isdir(outdir):
             if os.system("test -d %s" % outdir):
                 log.error("value [%s] of key [%s] is not a directory" % (outdir,self.DATASET_DIR))
                 sys.exit(4)
-        if info.get('QUIET', '').upper() in TRUES:
+        
+        if info['QUIET'].upper() in TRUES:
             voption = ''
         else:
             voption = '-v '
-        if prefix == 'getmsdata' and not info.get('KEEP_NAME', '').upper() in TRUES:
+        
+        if prefix == 'getmsdata' and not info['KEEP_NAME'].upper() in TRUES:
             print 'change name on'
             koption = '-c '
         else:
             koption = ''
-        self._failure_tolerant = info.get('FAILURE_TOLERANT', '').upper() in TRUES
+        
+        self._failure_tolerant = info['FAILURE_TOLERANT'].upper() in TRUES
+        
         print prefix
         print outdir
         print self._codes
@@ -102,25 +91,25 @@ class Dss(IWrapper):
                 prefix, outdir, self._result_filename, 
                 voption, koption,  " ".join(self._codes))
         return command, info
-       
+    
+    def set_args(self,log,args_handler):
+        """
+        See interface
+        """
+        args_handler.add_app_args(log, self.DATASET_DIR, 'download directory')
+        args_handler.add_app_args(log, self.DATASET_CODE, 'data set code')
+        args_handler.add_app_args(log, self.DSSKEYS, "controls the output. results are values of these keys. "+
+                                                     "the default is 'DSSOUTPUT' plus potentially a prefix depending key", default='')
+        args_handler.add_app_args(log, self.PREFIX, "one of these executables: %s" % self.ALLOWED_PREFIXES, choices=self.ALLOWED_PREFIXES)
+        args_handler.add_app_args(log, 'QUIET', "no messages written to stdout if set to true", default='False')
+        args_handler.add_app_args(log, 'KEEP_NAME', "for prefix is 'gegmsdata' only: output keeps original file name if set to true "+
+                                                       "(otherwise it will be changed to samplecode~dscode.mzXXML)", default='False')
+        args_handler.add_app_args(log, 'FAILURE_TOLERANT', "exits with 0 in cases where no data set was found if to true", default='False')
+        return args_handler
+        
     def validate_run(self, info, log, run_code, out_stream, err_stream):
         """
-        Validate the execution of the external application. 
-        (e.g. output parsing)
-        
-        @type info: dict         
-        @param info: Dictionary object with information needed by the class
-        @type log: Logger 
-        @param log: Logger to store log messages 
-        @type run_code: int
-        @param run_code: Exit code of the process prepared with prepare_run() 
-        @type out_stream: Stream
-        @param out_stream: Stream object with the stdout of the executed process
-        @type err_stream: Stream 
-        @param err_stream: Stream object with the stderr of the executed process 
-        
-        @rtype: (int,dict)
-        @return: Tuple of 2 objects; the exit code and the (updated) info object. 
+        See interface
         """
         exit_code = 0
         for (stream, logf, logt) in [[out_stream, log.debug, 'OUTPUT'], [err_stream, log.error, 'ERROR']]:
@@ -137,12 +126,12 @@ class Dss(IWrapper):
                     try:
                         ds, fl = downloaded.split("\t")
                         dsfls.setdefault(ds, []).append(fl)
-                        self._log(log.debug, 'DEBUG', "add file '%s' for dataset %s to output" % (fl, ds))
+                        log.debug("add file '%s' for dataset %s to output" % (fl, ds))
                     except ValueError:
                         if not self._failure_tolerant:
                             raise NoDataSetFound(downloaded)
                         else:
-                            self._log(log.error, 'ERROR', "No data set found for %s" % downloaded)
+                            log.error("No data set found for %s" % downloaded)
             # for single code requests: either add single file or array
             if len(self._codes) == 1:
                 fstdsfls = dsfls.get(self._codes[0])
@@ -160,14 +149,3 @@ class Dss(IWrapper):
             log.critical("Validation of result file failed: %s %s" % (e.__class__.__name__, e))
             exit_code = 1
         return exit_code, info
-
-#if "__main__" == __name__:
-#    # init the application object (__init__)
-#    a = Dss(use_filesystem=True,name=None)
-#    # call the application object as method (__call__)
-#    exit_code = a(sys.argv)
-#    #copy the log file to the working dir
-#    for filename in [a._log_filename,a._stderr_filename,a._stdout_filename]:
-#        shutil.move(filename, os.path.join(a._wd,filename))
-#    print(exit_code)
-#    sys.exit(exit_code)        
