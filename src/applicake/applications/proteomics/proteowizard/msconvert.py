84a531aaca48596527feddaa2b2d807f7c6f15ea
quandt.andreas <quandt.andreas@a697f730-0300-b9c8-cee0-f846692decd2>
2012-06-14 13:16:57 +0000

    - work on ruffus_local_omssa (omssa + msconvert)
    git-svn-id: http://applicake.googlecode.com/svn/trunk@397 a697f730-0300-b9c8-cee0-f846692decd2

--- src/applicake/applications/proteomics/proteowizard/msconvert.py
+++ src/applicake/applications/proteomics/proteowizard/msconvert.py
@@ -4,7 +4,9 @@ Created on May 10, 2012
 @author: quandtan
 '''
 
+import os
 from applicake.framework.interfaces import IWrapper
+from applicake.framework.templatehandler import BasicTemplateHandler
 
 class Mzxml2Mgf(IWrapper):
     """
@@ -12,7 +14,10 @@ class Mzxml2Mgf(IWrapper):
     """
     
     def __init__(self):
-        self._default_prefix = 'msconert'
+        self._default_prefix = 'msconvert'
+        base = self.__class__.__name__
+        self._template_file = '%s.tpl' % base 
+        self._result_file = '%s.mgf' % base     
 
 
     def get_prefix(self,info,log):
@@ -21,12 +26,33 @@ class Mzxml2Mgf(IWrapper):
             log.debug('set [%s] to [%s] because it was not set before.' % (self.PREFIX,info[self.PREFIX]))
         return info[self.PREFIX],info  
     
+    def get_template_handler(self):
+        """
+        return Template handler
+        """
+        return Mzxml2MgfTemplate()    
+    
     def prepare_run(self,info,log):
         """
-        See interface
-        """
-         
-        return ('%s ' % (prefix),info)
+        See interface.
+        
+        - 
+                
+        @precondition: info object need the key [%s]
+        """ % self.TEMPLATE
+        wd = info[self.WORKDIR]
+        log.debug('reset path of application files from current dir to work dir [%s]' % wd)
+        self._template_file = os.path.join(wd,self._template_file) 
+        info['TEMPLATE'] = self._template_file 
+        self._result_file = os.path.join(wd,self._result_file) 
+        info['MGF'] = self._result_file  
+        log.debug('get template handler')
+        th = self.get_template_handler()
+        log.debug('modify template')                
+        mod_template,info = th.modify_template(info, log)        
+        prefix,info = self.get_prefix(info,log)
+        command = "%s %s --outfile %s -o %s -c %s" %(prefix,info['MZXML'],self._result_filename, wd,info['TEMPLATE']) 
+        return command,info  
     
     def set_args(self,log,args_handler):
         """
@@ -40,3 +66,26 @@ class Mzxml2Mgf(IWrapper):
         """
         See interface
         """
+        if 0 != run_code:
+            return run_code
+        return 0  
+
+class Mzxml2MgfTemplate(BasicTemplateHandler):
+    """
+    Template handler for Xtandem.  
+    """
+    def read_template(self, info, log):
+        """
+        See super class.
+        """
+        template = """# example configuration file
+mgf=true
+filter=titleMaker <RunId>.<ScanNumber>.<ScanNumber>.<ChargeState>
+
+#mzXML=true
+#zlib=true
+#filter="index [3,7]"
+#filter="precursorRecalculation"        
+        """        
+        log.debug('read template from [%s]' % self.__class__.__name__)
+        return template,info        
