854e304b30632700d8f011cefa0ec4f8aabdeee0
quandt.andreas <quandt.andreas@a697f730-0300-b9c8-cee0-f846692decd2>
2012-06-15 15:59:49 +0000

    - debugging proteomics.base (MzMlModifier/FeatureXmlModifier)
    git-svn-id: http://applicake.googlecode.com/svn/trunk@427 a697f730-0300-b9c8-cee0-f846692decd2

--- src/applicake/applications/proteomics/base.py
+++ src/applicake/applications/proteomics/base.py
@@ -197,104 +197,20 @@ class SingleFileTypeModifier(OpenMs):
 class IdXmlModifier(SingleFileTypeModifier):
     """
     Base class specifically for OpenMS tools that modify idXML files (e.g. for peptide-protein processing).
-    """
-    
+    """    
     _file_type = 'idXML'
 
-#    def __init__(self):
-#        """
-#        Constructor
-#        """
-#        super(IdXmlModifier,self).__init__()
-#        base = self.__class__.__name__
-#        self._result_file = '%s.idXML' % base # result produced by the application
-#
-#    def prepare_run(self,info,log):
-#        """
-#        See interface.
-#
-#        - Read the a specific template and replaces variables from the info object.
-#        - Tool is executed using the pattern: [PREFIX] -ini [TEMPLATE].
-#        - If there is a result file, it is added with a specific key to the info object.
-#        """
-#        wd = info[self.WORKDIR]
-#        log.debug('reset path of application files from current dir to work dir [%s]' % wd)
-#        self._template_file = os.path.join(wd,self._template_file)
-#        info['TEMPLATE'] = self._template_file
-#        self._result_file = os.path.join(wd,self._result_file)
-#        # have to temporarily set a key in info to store the original IDXML
-#        info['ORGIDXML'] = info['IDXML']
-#        info['IDXML'] = self._result_file
-#        log.debug('get template handler')
-#        th = self.get_template_handler()
-#        log.debug('modify template')
-#        mod_template,info = th.modify_template(info, log)
-#        # can delete temporary key as it is not longer needed
-#        del info['ORGIDXML']
-#        prefix,info = self.get_prefix(info,log)
-#        command = '%s -ini %s' % (prefix,self._template_file)
-#        return command,info
-#
-#    def set_args(self,log,args_handler):
-#        """
-#        See interface
-#        """
-#        args_handler = super(IdXmlModifier, self).set_args(log,args_handler)
-#        args_handler.add_app_args(log, 'IDXML', 'The input idXML file ')
-#        return args_handler
+
     
-class MzMlModifier(OpenMs):
+class MzMlModifier(SingleFileTypeModifier):
     """
     Base class specifically for OpenMS tools that modify mzML files (e.g. for signal processing)
-    """
-    
+    """   
     _file_type = 'mzML'
+        
 
-#    def __init__(self):
-#        """
-#        Constructor
-#        """
-#        super(MzMlModifier,self).__init__()
-#        base = self.__class__.__name__
-#        self._result_file = '%s.mzML' % base # result produced by the application
-#
-#    def prepare_run(self,info,log):
-#        """
-#        See interface.
-#
-#        - Read the a specific template and replaces variables from the info object.
-#        - Tool is executed using the pattern: [PREFIX] -ini [TEMPLATE].
-#        - If there is a result file, it is added with a specific key to the info object.
-#        """
-#        wd = info[self.WORKDIR]
-#        log.debug('reset path of application files from current dir to work dir [%s]' % wd)
-#        self._template_file = os.path.join(wd,self._template_file)
-#        info['TEMPLATE'] = self._template_file
-#        self._result_file = os.path.join(wd,self._result_file)
-#        # have to temporarily set a key in info to store the original IDXML
-#        info['ORGMZML'] = info['MZML']
-#        info['MZML'] = self._result_file
-#        log.debug('get template handler')
-#        th = self.get_template_handler()
-#        log.debug('modify template')
-#        mod_template,info = th.modify_template(info, log)
-#        # can delete temporary key as it is not longer needed
-#        del info['ORGMZML']
-#        prefix,info = self.get_prefix(info,log)
-#        command = '%s -ini %s' % (prefix,self._template_file)
-#        return command,info
-#
-#    def set_args(self,log,args_handler):
-#        """
-#        See interface
-#        """
-#        args_handler = super(MzMlModifier, self).set_args(log,args_handler)
-#        args_handler.add_app_args(log, 'MZML', 'The input mzML file ')
-#        return args_handler    
-
-class FeatureXmlModifier(OpenMs):
+class FeatureXmlModifier(SingleFileTypeModifier):
     """
     Base class specifically for OpenMS tools that modify featureXML files (e.g. for signal processing)
-    """
-    
+    """   
     _file_type = 'featureXML'
