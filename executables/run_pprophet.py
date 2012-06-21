95db5b40aa61689b8895888e5395871369125780
blum@imsb.biol.ethz.ch <blum@imsb.biol.ethz.ch@a697f730-0300-b9c8-cee0-f846692decd2>
2012-06-21 13:40:25 +0000

    run ppproph
    git-svn-id: http://applicake.googlecode.com/svn/trunk@522 a697f730-0300-b9c8-cee0-f846692decd2

--- executables/run_pprophet.py
+++ executables/run_pprophet.py
@@ -10,7 +10,7 @@ from applicake.framework.runner import WrapperRunner
 from applicake.applications.proteomics.tpp.proteinprophet import ProteinProphet
 
 runner = WrapperRunner()
-application = Omssa()
-exit_code = ProteinProphet(sys.argv,application)
+application = ProteinProphet()
+exit_code = runner(sys.argv,application)
 print exit_code
 sys.exit(exit_code)
