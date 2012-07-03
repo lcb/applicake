ff8aaa0c6114ee082a1e6d34695c74f0446e4d58
blum@imsb.biol.ethz.ch <blum@imsb.biol.ethz.ch@a697f730-0300-b9c8-cee0-f846692decd2>
2012-07-03 11:55:11 +0000

    git-svn-id: http://applicake.googlecode.com/svn/trunk@582 a697f730-0300-b9c8-cee0-f846692decd2

--- executables/run_copy2identdropbox.py
+++ executables/run_copy2identdropbox.py
@@ -6,10 +6,10 @@ Created on Jun 19, 2012
 '''
 
 import sys
-from applicake.framework.runner import ApplicationRunner
+from applicake.framework.runner import IniFileRunner
 from applicake.applications.proteomics.openbis.dropbox import Copy2IdentDropbox
 
-runner = ApplicationRunner()
+runner = IniFileRunner()
 application = Copy2IdentDropbox()
 exit_code = runner(sys.argv,application)
 print exit_code
