e427335f592129669e19dee8f3b7f76cbce881b3
quandt.andreas <quandt.andreas@a697f730-0300-b9c8-cee0-f846692decd2>
2012-07-03 12:07:03 +0000

    - ruffus_local_mascotapdapteronline: update to current status
    git-svn-id: http://applicake.googlecode.com/svn/trunk@583 a697f730-0300-b9c8-cee0-f846692decd2

--- executables/ruffus_local_mascotadapteronline.py
+++ executables/ruffus_local_mascotadapteronline.py
@@ -4,7 +4,6 @@ Created on Jun 24, 2012
 
 @author: quandtan
 '''
-
 import os
 import sys
 from ruffus import *
@@ -14,13 +13,14 @@ from subprocess import PIPE
 from applicake.framework.runner import IniFileRunner, ApplicationRunner
 from applicake.framework.runner import CollectorRunner
 from applicake.framework.runner import WrapperRunner
-from applicake.applications.commons.generator import DatasetcodeGenerator
+from applicake.applications.commons.generator import DatasetcodeGenerator 
 from applicake.applications.os.echo import Echo
 from applicake.applications.commons.collector import GuseCollector
 from applicake.applications.proteomics.searchengine.xtandem import Xtandem
 from applicake.applications.proteomics.openbis.dss import Dss
 from applicake.applications.proteomics.tpp.tandem2xml import Tandem2Xml
 from applicake.applications.proteomics.tpp.xinteract import Xinteract
+from applicake.applications.proteomics.tpp.refreshparser import RefreshParser
 from applicake.applications.proteomics.tpp.interprophet import InterProphet
 from applicake.applications.proteomics.openms.filehandling.idfileconverter import PepXml2IdXml,\
     IdXml2PepXml
@@ -41,7 +41,6 @@ from applicake.applications.commons.inifile import Unifier
 from applicake.framework.interfaces import IApplication, IWrapper
 from applicake.applications.proteomics.searchengine.myrimatch import Myrimatch
 from applicake.applications.proteomics.openms.identification.macotadapteronline import MascotAdapterOnline
-from applicake.applications.proteomics.tpp.refreshparser import RefreshParser
 
 cwd = None
 
@@ -68,151 +67,20 @@ def wrap(applic,  input_file_name, output_file_name,opts=None):
 
 def execute(command):
     p = Popen(command, shell=True, stdout=PIPE, stderr=PIPE)            
-    output, error = p.communicate()                                                                                                                                                                            
+    output, error = p.communicate()                                                                                                                                                                              
     out_stream = StringIO(output)
     err_stream = StringIO(error) 
 
 
-def setup():
-    cwd = '.'
-    os.chdir(cwd)
-    execute("find . -type d -iname '[0-9]*' -exec rm -rf {} \;")
-    execute('rm *.err')
-    execute('rm *.out')
-    execute('rm *.log')
-    execute('rm *ini*')
-#    execute('rm jobid.txt') 
-    execute('rm flowchart.*')    
-    with open("input.ini", 'w+') as f:
-        f.write("""BASEDIR = /cluster/scratch/malars/workflows
-LOG_LEVEL = DEBUG
-STORAGE = file
-TEMPLATE = template.tpl
-DATASET_DIR = /cluster/scratch/malars/datasets
-DATASET_CODE = 20120124102254267-296925,
-DBASE = /cluster/scratch/malars/biodb/ex_sp/current/decoy/ex_sp_9606.fasta
-DECOY_STRING = DECOY_ 
-FRAGMASSERR = 0.4
-FRAGMASSUNIT = Da
-PRECMASSERR = 15
-PRECMASSUNIT = ppm
-MISSEDCLEAVAGE = 0
-ENZYME = Trypsin
-STATIC_MODS = Carbamidomethyl (C)
-THREADS = 4
-XTANDEM_SCORE = k-score
-XINTERACT_ARGS = -dDECOY_ -OAPdlIw
-IPROPHET_ARGS = MINPROB=0
-FDR=0.01
-SPACE = QUANDTAN
-PROJECT = TEST
-DROPBOX = /cluster/scratch/malars/drop-box_prot_ident
-""" 
-#,20120603165413998-510432,
-# 20120606045538225-517638 -> b10-01219.p.mzxml
-# 20120603160111752-510155 -> b10-01219.c.mzxml 
-# 20120124102254267-296925,20120124121656335-296961 -> orbi silac hela from petri
-# 20120223153200939-319955 -> 100422_JM_10P2_03
-)       
-        
+def idxml2pepxml():
+    wrap(IdXml2PepXml,'mascotadapteronline.ini','idxml2pepxml.ini')
 
-@follows(setup)
-@split("input.ini", "generate.ini_*")
-def generator(input_file_name, notused_output_file_names):
-    argv = ['', '-i', input_file_name, '--GENERATORS', 'generate.ini','-o','generator.ini','-l','DEBUG']
-    runner = IniFileRunner()
-    application = GuseDatasetcodeGenerator()
-    exit_code = runner(argv, application)
-    if exit_code != 0:
-        raise Exception("generator failed [%s]" % exit_code) 
-    
-@transform(generator, regex("generate.ini_"), "dss.ini_")
-def dss(input_file_name, output_file_name):   
-    wrap(Dss,input_file_name, output_file_name,['--PREFIX', 'getmsdata'])
+@follows(idxml2pepxml)
+def refreshparser():
+    wrap(RefreshParser,'idxml2pepxml.ini','refreshparser.ini',['-p'])   
 
-@transform(dss, regex("dss.ini_"), "mzxml2mzml.ini_")
-def mzxml2mzml(input_file_name, output_file_name):
-    wrap(Mzxml2Mzml,input_file_name, output_file_name,['-s','file','-l','DEBUG'])
-    
-@transform(mzxml2mzml, regex("mzxml2mzml.ini_"), "mascotadapteronline.ini_")
-def mascotadapteronline(input_file_name, output_file_name):
-    wrap(MascotAdapterOnline,input_file_name, output_file_name,
-         ['--MASCOT_HOSTNAME','imsb-ra-mascot.ethz.ch','--MASCOT_USERNAME','bla','--MASCOT_PASSWORD','blabla' '-p'])
-
-@transform(mascotadapteronline, regex("mascotadapteronline.ini_"), "idxml2pepxml.ini_")
-def idxml2pepxml(input_file_name, output_file_name):
-    wrap(IdXml2PepXml,input_file_name, output_file_name,['-p'])
-
-@transform(idxml2pepxml, regex("idxml2pepxml_"), "refreshparser.ini_")
-def refreshparser(input_file_name, output_file_name):
-    wrap(RefreshParser,input_file_name, output_file_name)   
-
-    
-@merge(refreshparser, "collector.ini")
-def collector(notused_input_file_names, output_file_name):
-    argv = ['', '--COLLECTORS', 'refreshparser.ini', '-o', output_file_name,'-s','file']
-    runner = CollectorRunner()
-    application = GuseCollector()
-    exit_code = runner(argv, application)
-    if exit_code != 0:
-        raise Exception("[%s] failed [%s]" % ('collector',exit_code))    
-
-@follows(collector)
-def unifier():
-    argv = ['', '-i', 'collector.ini', '-o','unifier.ini','-p','--UNIFIER_REDUCE']
-    runner = IniFileRunner()
-    application = Unifier()
-    exit_code = runner(argv, application)
-    if exit_code != 0:
-        raise Exception("unifier [%s]" % exit_code)  
-
-@follows(unifier)
+@follows(refreshparser)
 def interprophet():
-    wrap(InterProphet,'unifier.ini','interprophet.ini')    
+    wrap(InterProphet,'refreshparser.ini','interprophet.ini')  
 
-@follows(interprophet)
-def pepxml2csv():
-    wrap(Pepxml2Csv,'interprophet.ini','pepxml2csv.ini')   
-    
-@follows(pepxml2csv)
-def fdr2probability():
-    wrap(Fdr2Probability,'pepxml2csv.ini','fdr2probability.ini')         
-
-@follows(fdr2probability)
-def proteinprophet():
-    wrap(ProteinProphet,'fdr2probability.ini','proteinprophet.ini') 
-
-@follows(proteinprophet)
-def protxml2spectralcount():
-    wrap(ProtXml2SpectralCount,'proteinprophet.ini','protxml2spectralcount.ini') 
-
-@follows(protxml2spectralcount)
-def protxml2modifications():
-    wrap(ProtXml2Modifications,'protxml2spectralcount.ini','protxml2modifications.ini') 
-
-@follows(protxml2modifications)
-def protxml2openbis():
-    wrap(ProtXml2Openbis,'protxml2modifications.ini','protxml2openbis.ini') 
-
-@follows(protxml2openbis)
-def copy2dropbox():
-    wrap(Copy2Dropbox,'protxml2openbis.ini','copy2dropbox.ini') 
-
-#@follows()
-#def ():
-#    wrap(,'','') 
-#    @follows()
-#def ():
-#    wrap(,'','') 
-#    @follows()
-#def ():
-#    wrap(,'','')     
-
-
-
-
-pipeline_run([copy2dropbox])
-#pipeline_run([featurefindercentroided])
-
-
-#pipeline_printout_graph ('flowchart.png','png',[collector],no_key_legend = False) #svg
+pipeline_run([interprophet])
