7ffeef80f3621e0196e07387295f55bcd6ae791c
blum@imsb.biol.ethz.ch <blum@imsb.biol.ethz.ch@a697f730-0300-b9c8-cee0-f846692decd2>
2012-07-05 10:29:44 +0000

    all the best from C77
    git-svn-id: http://applicake.googlecode.com/svn/trunk@593 a697f730-0300-b9c8-cee0-f846692decd2

--- src/applicake/framework/interfaces.py
+++ src/applicake/framework/interfaces.py
@@ -137,14 +137,6 @@ class IWrapper(KeyEnum):
     Interface for application that wraps an external application
     """
         
-    def get_template_handler(self):
-        """
-        Interface method to inject specific templates.
-        
-        @rtype: applicake.framework.interfaces.ITemplateHandler
-        @return: Specific template handler used for the tool
-        """
-        raise NotImplementedError("get_template_handler() is not implemented")        
         
     def prepare_run(self,info,log):
         """
