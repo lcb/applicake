9f1818d8de20a5eee60094a55940343bc78b2631
quandt.andreas <quandt.andreas@a697f730-0300-b9c8-cee0-f846692decd2>
2012-07-06 10:59:42 +0000

    git-svn-id: http://applicake.googlecode.com/svn/trunk@634 a697f730-0300-b9c8-cee0-f846692decd2

--- executables/ruffus_local_xtandem.py
+++ executables/ruffus_local_xtandem.py
@@ -82,7 +82,7 @@ def setup():
 LOG_LEVEL = DEBUG
 STORAGE = file
 TEMPLATE = template.tpl
-DATASET_DIR = /cluster/home/biol/quandtan/test/datasets
+DATASET_DIR = /cluster/scratch/malars/datasets
 DATASET_CODE = 20120124102254267-296925,20120124121656335-296961
 DBASE = /cluster/scratch/malars/biodb/ex_sp/current/decoy/ex_sp_9606.fasta
 DECOY_STRING = DECOY_ 
