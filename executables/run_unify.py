60811f32db2993368386799b1e01822b61d2fa42
blum@imsb.biol.ethz.ch <blum@imsb.biol.ethz.ch@a697f730-0300-b9c8-cee0-f846692decd2>
2012-07-09 14:09:03 +0000

    run unify inirunner2
    git-svn-id: http://applicake.googlecode.com/svn/trunk@677 a697f730-0300-b9c8-cee0-f846692decd2

--- executables/run_unify.py
+++ executables/run_unify.py
@@ -6,10 +6,10 @@ Created on May 24, 2012
 '''
 
 import sys
-from applicake.framework.runner import IniFileRunner
+from applicake.framework.runner import IniFileRunner2
 from applicake.applications.commons.inifile import Unifier
 
-runner = IniFileRunner()
+runner = IniFileRunner2()
 application = Unifier()
 exit_code = runner(sys.argv,application)
 print exit_code
