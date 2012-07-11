57e43df7789658af159de2842f74b3f4629f29e3
quandt.andreas <quandt.andreas@a697f730-0300-b9c8-cee0-f846692decd2>
2012-07-11 18:26:35 +0000

    git-svn-id: http://applicake.googlecode.com/svn/trunk@694 a697f730-0300-b9c8-cee0-f846692decd2

--- executables/ruffus_local_openswath.py
+++ executables/ruffus_local_openswath.py
@@ -75,21 +75,19 @@ MIN_COVERAGE = 0.6
 )       
         
 
-#@follows(setup)
-#def chromatogramextractor():
-#    wrap(ChromatogramExtractor,'input.ini','chromatogramextractor.ini',['-p']) 
-#    
-#@follows(chromatogramextractor)
-#def mrmnormalizer():
-#    wrap(MRMNormalizer,'chromatogramextractor.ini','mrmnormalizer.ini',['-p'])     
-#
-#@follows(mrmnormalizer)
-#def chromatogramextractor2():
-#    wrap(ChromatogramExtractor,'chromatogramextractor.ini','chromatogramextractor2.ini',['-p']) 
-#
-#@follows(chromatogramextractor2)
-
 @follows(setup)
+def chromatogramextractor():
+    wrap(ChromatogramExtractor,'input.ini','chromatogramextractor.ini',['-p']) 
+    
+@follows(chromatogramextractor)
+def mrmnormalizer():
+    wrap(MRMNormalizer,'chromatogramextractor.ini','mrmnormalizer.ini',['-p'])     
+
+@follows(mrmnormalizer)
+def chromatogramextractor2():
+    wrap(ChromatogramExtractor,'chromatogramextractor.ini','chromatogramextractor2.ini',['-p']) 
+
+@follows(chromatogramextractor2)
 def mrmanalyzer():
     wrap(MRMAnalyzer,'chromatogramextractor2.ini','mrmanalyzer.ini',['-p']) 
 
