c06f08741c82b1396fbaac4cbed269db9a0339d0
wewolski@gmail.com <wewolski@gmail.com@a697f730-0300-b9c8-cee0-f846692decd2>
2012-08-14 13:51:20 +0000

    file merger added
    git-svn-id: http://applicake.googlecode.com/svn/trunk@720 a697f730-0300-b9c8-cee0-f846692decd2

---
+++ src/applicake/applications/proteomics/openswath/FileMerger.py
@@ -0,0 +1,72 @@
+'''
+Created on Aug 14, 2012
+
+@author: blum, wolski
+'''
+
+import os
+from applicake.framework.interfaces import IWrapper
+from applicake.framework.templatehandler import BasicTemplateHandler
+from applicake.utils.fileutils import FileUtils
+from applicake.utils.xmlutils import XmlValidator
+
+class FileMerger(IWrapper):
+    '''
+    Wrapper for the ChromatogramExtractor of OpenSWATH.
+    '''
+    _template_file = ''
+    _result_file = ''
+    _default_prefix = 'FileMerger'
+
+
+    def get_prefix(self,info,log):
+        if not info.has_key(self.PREFIX):
+            info[self.PREFIX] = self._default_prefix
+            log.debug('set [%s] to [%s] because it was not set before.' % (self.PREFIX,info[self.PREFIX]))
+        return info[self.PREFIX],info
+
+    def prepare_run(self,info,log):
+        """
+        See interface.
+
+        - Define path to result file (depending on work directory)
+        - If a template is used, the template is read variables from the info object are used to set concretes.
+        - If there is a result file, it is added with a specific key to the info object.
+        """
+        key = 'CHROM_MZML' #self._file_type.upper()
+        wd = info[self.WORKDIR]
+        log.debug('reset path of application files from current dir to work dir [%s]' % wd)
+        self._result_file = os.path.join(wd,self._result_file)
+        info[key] = self._result_file
+        prefix,info = self.get_prefix(info,log)
+        command = '%s -in %s -out %s' % (prefix, info['CHROMMZML'], info['OUTCHROMMZML'])
+        return command,info
+
+    def set_args(self,log,args_handler):
+        """
+        See super class.
+
+        """
+        args_handler.add_app_args(log, self.WORKDIR, 'Directory to store files')
+        args_handler.add_app_args(log, self.PREFIX, 'Path to the executable')
+        args_handler.add_app_args(log, self.COPY_TO_WD, 'List of files to store in the work directory') 
+        args_handler.add_app_args(log, 'INCHROMMZML', 'chrom.mzml files to merge.') 
+        args_handler.add_app_args(log, 'OUTCHROMMZML', 'merged chrom.mzml file') 
+        
+        return args_handler
+
+    def validate_run(self,info,log, run_code,out_stream, err_stream):
+        """
+        See super class.
+        """
+        if 0 != run_code:
+            return run_code,info
+    #out_stream.seek(0)
+    #err_stream.seek(0)
+        if not FileUtils.is_valid_file(log, info['OUTCHROMMZML']):
+            log.critical('[%s] is not valid' %info['OUTCHROMMZML'])
+            return 1,info
+        if not XmlValidator.is_wellformed(info['OUTCHROMMZML']):
+            log.critical('[%s] is not well formed.' %info['OUTCHROMMZML'])
+            return 1,info    
+        return 0,info
