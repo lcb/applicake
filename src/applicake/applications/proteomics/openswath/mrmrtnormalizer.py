90c26e7271e6293d155eb1ce02f333a106ec1579
wewolski@gmail.com <wewolski@gmail.com@a697f730-0300-b9c8-cee0-f846692decd2>
2012-08-20 13:14:33 +0000

    fixing
    git-svn-id: http://applicake.googlecode.com/svn/trunk@746 a697f730-0300-b9c8-cee0-f846692decd2

--- src/applicake/applications/proteomics/openswath/mrmrtnormalizer.py
+++ src/applicake/applications/proteomics/openswath/mrmrtnormalizer.py
@@ -48,8 +48,8 @@ class MRMRTNormalizer(IWrapper):
         info[key] = self._result_file
         prefix,info = self.get_prefix(info,log)
         command = '%s -in %s -tr %s -min_rsq %s -min_coverage %s -threads %s -out %s' % (prefix,
-                                                                                         info['CHROM_MZML'],
-                                                                                         info['TRAML'],
+                                                                                         info['OUTCHROMMZML'],
+                                                                                         info['IRTTRAML'],
                                                                                          info['MIN_RSQ'],
                                                                                          info['MIN_COVERAGE'],
                                                                                          info['THREADS'],
@@ -65,7 +65,7 @@ class MRMRTNormalizer(IWrapper):
         args_handler.add_app_args(log, self.COPY_TO_WD, 'List of files to store in the work directory') 
         args_handler.add_app_args(log, 'THREADS', 'Number of threads used in the process.') 
         args_handler.add_app_args(log, 'TRAML', 'Path to the TraML file.')
-        args_handler.add_app_args(log, 'CHROM_MZML', 'Path to the chrom.mzML files.')
+        args_handler.add_app_args(log, 'OUTCHROMMZML', 'Path to the chrom.mzML files.')
         args_handler.add_app_args(log, 'MIN_RSQ', '') 
         args_handler.add_app_args(log, 'MIN_COVERAGE', '')
         #args_handler.add_app_args(log, '', '')
