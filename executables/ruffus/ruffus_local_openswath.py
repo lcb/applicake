5ff1386e75be4a55849569de75ea3ef12b99b3b7
blum@imsb.biol.ethz.ch <blum@imsb.biol.ethz.ch@a697f730-0300-b9c8-cee0-f846692decd2>
2012-08-20 08:54:49 +0000

    openswath
    git-svn-id: http://applicake.googlecode.com/svn/trunk@729 a697f730-0300-b9c8-cee0-f846692decd2

--- executables/ruffus/ruffus_local_openswath.py
+++ executables/ruffus/ruffus_local_openswath.py
@@ -9,12 +9,11 @@ Created on Jul 11, 2012
 
 import os
 import sys
-from ruffus import *
-from cStringIO import StringIO
-from subprocess import Popen
-from subprocess import PIPE
-from applicake.framework.runner import IniFileRunner2, ApplicationRunner, CollectorRunner, WrapperRunner, IniFileRunner
+import subprocess
 
+from ruffus import *
+
+from applicake.framework.runner import IniFileRunner2, ApplicationRunner, CollectorRunner, WrapperRunner, IniFileRunner
 from applicake.framework.interfaces import IApplication, IWrapper
 from applicake.applications.proteomics.openswath.chromatogramextractor import ChromatogramExtractor
 from applicake.applications.proteomics.openswath.mrmrtnormalizer import MRMRTNormalizer
@@ -45,32 +44,18 @@ def wrap(applic, input_file_name, output_file_name, opts=None):
     if exit_code != 0:
         raise Exception("[%s] failed [%s]" % (applic.__name__, exit_code)) 
 
-def execute(command):
-    p = Popen(command, shell=True, stdout=PIPE, stderr=PIPE)            
-    output, error = p.communicate()                                                                                                                                                                            
-    out_stream = StringIO(output)
-    err_stream = StringIO(error) 
-
 
 def setup():
-    cwd = '.'
-    os.chdir(cwd)
-    execute("find . -type d -iname '[0-9]*' -exec rm -rf {} \;")
-    execute('rm *.err')
-    execute('rm *.out')
-    execute('rm *.log')
-    execute('rm *ini*')
-
-#    execute('rm jobid.txt') 
-    execute('rm flowchart.*')    
+    subprocess.call("rm *ini* *.err *.out *.log ",shell=True)
     with open("input.ini", 'w+') as f:
-        f.write("""BASEDIR = /cluster/home/biol/wwolski/imsb/ruffus
+        f.write("""BASEDIR = /cluster/scratch/malars/loblum/openswathtest/workflows
 LOG_LEVEL = DEBUG
-STORAGE = memmory_all
+STORAGE = memory_all
 THREADS = 8
-MZMLGZDIR = /cluster/scratch/malars/openswath/data/AQUA_fixed_water/split_napedro_L120224_001_SW-400AQUA_no_background_2ul_dilution_10
-CHROMEXTRACTOR1.TRAML = "/cluster/scratch/malars/openswath/assays/iRT/DIA_iRT.TraML"
-CHROMEXTRACTOR2.TRAML = "/cluster/scratch/malars/openswath/assays/AQUA/AQUA4_sh.TraML"
+MZMLGZDIR = /cluster/scratch/malars/loblum/openswathtest/datasets
+TRAML = "/cluster/scratch/malars/loblum/openswathtest/tramls/DIA_iRT.TraML"
+CHROMEXTRACTOR1.TRAML = "/cluster/scratch/malars/loblum/openswathtest/tramls/DIA_iRT.TraML"
+CHROMEXTRACTOR2.TRAML = "/cluster/scratch/malars/loblum/openswathtest/tramls/AQUA4_sh_new.TraML"
 MIN_UPPER_EDGE_DIST = 1
 MIN_RSQ = 0.95
 MIN_COVERAGE = 0.6
@@ -80,7 +65,7 @@ MIN_COVERAGE = 0.6
 @follows(setup)
 @split("input.ini", "generate.ini_*")
 def generator(input_file_name, notused_output_file_names):
-    argv = ['', '-i', input_file_name, '--GENERATORS', 'generate.ini','-o','generator.ini','-l','DEBUG']
+    argv = ['', '-i', input_file_name, '--GENERATORS', 'generate.ini']
     runner = IniFileRunner()
     application = SwathGenerator()
     exit_code = runner(argv, application)
@@ -90,7 +75,7 @@ def generator(input_file_name, notused_output_file_names):
 
 @transform(generator, regex("generate.ini_"), "chromatogramextractor.ini_")
 def chromatogramextractor(input_file_name, output_file_name):
-    wrap(ChromatogramExtractor, input_file_name, output_file_name, ['-p']) 
+    wrap(ChromatogramExtractor, input_file_name, output_file_name) 
 
 
 @follows(chromatogramextractor)
@@ -127,6 +112,4 @@ def mrmanalyzer(input_file_name, output_file_name):
 def featurexmltotsv(input_file_name, output_file_name):
     wrap(FeatureXMLToTSV, input_file_name, output_file_name, ['-p']) 
 
-pipeline_run([featurexmltotsv], multiprocess=1)
-
-#pipeline_printout_graph ('flowchart.png','png',[copy2dropbox],no_key_legend = False) #svg
+pipeline_run([featurexmltotsv], multiprocess=2)
