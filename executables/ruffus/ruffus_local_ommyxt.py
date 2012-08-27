fa1767d968fcb2b1454e10c8b1e7d7e7f44cc89f
blum@imsb.biol.ethz.ch <blum@imsb.biol.ethz.ch@a697f730-0300-b9c8-cee0-f846692decd2>
2012-08-27 14:52:20 +0000

    ommyxt - coallate
    git-svn-id: http://applicake.googlecode.com/svn/trunk@780 a697f730-0300-b9c8-cee0-f846692decd2

--- executables/ruffus/ruffus_local_ommyxt.py
+++ executables/ruffus/ruffus_local_ommyxt.py
@@ -104,7 +104,7 @@ WORKFLOW= ruffus_local_ommyxt tina
 )       
         
 
-#@follows(setup)
+@follows(setup)
 @split("input.ini", "generate.ini_*")
 def generator(input_file_name, notused_output_file_names):
     argv = ['', '-i', input_file_name, '--GENERATORS', 'generate.ini','-o','generator.ini']
