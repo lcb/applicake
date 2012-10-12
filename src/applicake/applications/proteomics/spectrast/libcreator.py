2fb25d5381923ac43f5de15a611561ce995f5b2f
quandt.andreas <quandt.andreas@a697f730-0300-b9c8-cee0-f846692decd2>
2012-10-12 21:30:30 +0000

    git-svn-id: http://applicake.googlecode.com/svn/trunk@995 a697f730-0300-b9c8-cee0-f846692decd2

--- src/applicake/applications/proteomics/spectrast/libcreator.py
+++ src/applicake/applications/proteomics/spectrast/libcreator.py
@@ -127,7 +127,21 @@ class NoDecoyLibrary(LibraryCreator):
         args_handler.add_app_args(log, self.SPLIB, 'Spectrast library in .splib format')
         return args_handler
 
-#spectrast -cf'Protein !~ REV_  &  Protein !~ DECOY_' -cNdecoy_removed raw.splib >spectrast_decoyremoved.log
+
+class ConsensusLibrary(LibraryCreator):
+    
+    def get_suffix(self,info,log):
+        root = os.path.splitext(self._result_file1)[0] 
+        return "-cf'Protein !~ REV_  &  Protein !~ DECOY_' -cN%s %s" % (root,self._orig_splib)
+
+    def set_args(self,log,args_handler):
+        """
+        See interface
+        """
+        args_handler = super(ConsensusLibrary, self).set_args(log,args_handler)
+        args_handler.add_app_args(log, self.SPLIB, 'Spectrast library in .splib format')
+        return args_handler
+
 
 class LibraryCreatorTemplate(BasicTemplateHandler):
     """
