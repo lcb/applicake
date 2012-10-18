564c8816307db20e614a7e623436771d859a0493
blum@imsb.biol.ethz.ch <blum@imsb.biol.ethz.ch@a697f730-0300-b9c8-cee0-f846692decd2>
2012-10-18 11:36:39 +0000

    processexperiment - getcodes
    git-svn-id: http://applicake.googlecode.com/svn/trunk@1055 a697f730-0300-b9c8-cee0-f846692decd2

--- src/applicake/applications/proteomics/openbis/processexperiment.py
+++ src/applicake/applications/proteomics/openbis/processexperiment.py
@@ -11,6 +11,7 @@ class ProcessExperiment(IApplication):
 
     def set_args(self,log,args_handler):
         args_handler.add_app_args(log, self.SEARCH, 'Key where containing files of downloaded experiment')
+        args_handler.add_app_args(log, 'GETCODES', 'Get DSS codes from experiment properties', default=False)
         return args_handler
 
     def main(self,info,log):
@@ -21,7 +22,9 @@ class ProcessExperiment(IApplication):
                 info[self.PROTXML] = entry
             if entry.endswith('pep.xml'):
                 info[self.PEPXMLS] = [entry]
-
+            if entry.endswith('.properties'):
+                propfile = entry
+                                                                            
         run_code = 0
         if not self.PROTXML in info:
             log.fatal("No prot xml file was found")
@@ -29,6 +32,15 @@ class ProcessExperiment(IApplication):
         if not self.PEPXMLS in info:
             log.fatal("No pep xml file was found")
             run_code = 1
+         
+        if info['GETCODES'] == 'True': 
+            with open(propfile) as prop:
+                for line in prop.readlines():
+                    if line.startswith("PARENT-DATA-SET-CODES"):
+                        info[self.DATASET_CODE] = line.split('=')[1].strip().split(', ')
+            if self.DATASET_CODE not in info:
+                log.fatal("No search properties file was found in experiment folder")
+                run_code = 1
         
         #remove these guys to prevent parameter sweep
         info[self.SEARCH] = None
