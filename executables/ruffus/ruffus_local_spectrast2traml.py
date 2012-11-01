5a4de2b0c314e8f7cdda4d54a430ac585dfc4f95
quandt.andreas <quandt.andreas@a697f730-0300-b9c8-cee0-f846692decd2>
2012-11-01 08:58:25 +0000

    git-svn-id: http://applicake.googlecode.com/svn/trunk@1090 a697f730-0300-b9c8-cee0-f846692decd2

--- executables/ruffus/ruffus_local_spectrast2traml.py
+++ executables/ruffus/ruffus_local_spectrast2traml.py
@@ -157,7 +157,8 @@ def nodecoylib(input_file_name, output_file_name):
 
 @transform(nodecoylib,regex('nodecoylib.ini_'),'irtcalibration.ini_')
 def irtcalibration(input_file_name, output_file_name):
-    wrap(SpectrastIrtCalibrator,input_file_name, output_file_name)
+    wrap(SpectrastIrtCalibrator,input_file_name, output_file_name,['--PREFIX','/cluster/apps/openms/openswath-testing/mapdiv/scripts/assays/spectrast2spectrast_irt.py',
+                                                                   '-s','memory_all'])
 
 @transform(irtcalibration,regex('irtcalibration.ini_'),'consensuslib.ini_')
 def consensuslib(input_file_name, output_file_name):
