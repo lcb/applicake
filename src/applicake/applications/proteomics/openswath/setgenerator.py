28b81ffa2a010a1721f96faba236ff212271ec50
blum@imsb.biol.ethz.ch <blum@imsb.biol.ethz.ch@a697f730-0300-b9c8-cee0-f846692decd2>
2012-11-07 15:47:55 +0000

    openswath - ran through for 2parameters & 2datasets
    git-svn-id: http://applicake.googlecode.com/svn/trunk@1111 a697f730-0300-b9c8-cee0-f846692decd2

--- src/applicake/applications/proteomics/openswath/setgenerator.py
+++ src/applicake/applications/proteomics/openswath/setgenerator.py
@@ -26,38 +26,36 @@ class SetGenerator(Generator):
         if not isinstance(info[self.FILE_IDX],list):
             log.info('value [%s] of key [%s] was no list, converting' % (info[self.FILE_IDX],self.FILE_IDX))
             info[self.FILE_IDX] = [info[self.FILE_IDX]]
-			
+
         #remove SUBfile_idx as it is not longer needed
         basedic = info.copy()
         del basedic['SUBFILE_IDX']        
         file_dicts = []
-        if len(SequenceUtils.unify(basedic[self.FILE_IDX], reduce = True)) ==1:                        
-            for key in basedic.keys():
-                if isinstance(basedic[key], list) and key != self.INPUT:
-                    basedic[key] = SequenceUtils.unify(basedic[key], reduce = True)
-            file_dicts.append(basedic)            
-        else:
-            keys = basedic.keys()
-            log.debug('number of keys [%s]' % len(keys))
+        param_idxs = basedic[self.PARAM_IDX]
+        keys = basedic.keys()
+        for param_idx in SequenceUtils.unify(param_idxs, reduce=True):
             file_idxs = basedic[self.FILE_IDX]
-            # loops of unique list of found parameter idices
-            for file_idx in SequenceUtils.unify(file_idxs):
+            for file_idx in SequenceUtils.unify(file_idxs, reduce=True):
+                parampositions = SequenceUtils.get_indices(param_idxs, lambda x: x == param_idx)    
+                filepositions = SequenceUtils.get_indices(file_idxs, lambda x: x == file_idx)    
+                paramfilepositions = list(set(parampositions).intersection(filepositions))  
                 file_dict = {}
-                positions = SequenceUtils.get_indices(file_idxs, lambda x: x == file_idx)
                 for key in keys:
                     value = basedic[key]    
                     if not isinstance(value, list):
-                        log.info('found value %s is not a list', value)
                         file_dict[key] = value
-                    elif len(value) != len(file_idxs):
-                        log.info('length of value [%s] does not correspond to length of file indices [%s]. key possibly not coming from input file' % (value,len(file_idxs)))
+                        log.info('adding single value %s = %s' % (key,value))
+                    elif len(value) != len(param_idxs):
                         file_dict[key] = SequenceUtils.unify(value,reduce=True)
+                        log.info('adding strange list %s = %s' % (key, file_dict[key]))        
                     else:                
-                        values = [value[pos] for pos in positions]
-                        log.info('ADDING ' + str(SequenceUtils.unify(values,reduce=True)) + ' to ' + key)
+                        values = [value[pos] for pos in paramfilepositions]
                         file_dict[key] = SequenceUtils.unify(values,reduce=True)
+                        log.info('adding list %s = %s' % (key, file_dict[key]) )
+                        
                 log.info('param idx [%s] created dict [%s]' % (file_idx,file_dict))
-                file_dicts.append(file_dict)    
+                file_dicts.append(file_dict) 
+
         # write ini files
         info = self.write_files(basedic,log,file_dicts)
         return (0,info)           
