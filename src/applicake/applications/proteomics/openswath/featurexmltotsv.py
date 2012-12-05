3d9b5e8020e87fced8625a246a15652935d29ecd
blum@imsb.biol.ethz.ch <blum@imsb.biol.ethz.ch@a697f730-0300-b9c8-cee0-f846692decd2>
2012-12-05 11:32:07 +0000

    openswath
    git-svn-id: http://applicake.googlecode.com/svn/trunk@1169 a697f730-0300-b9c8-cee0-f846692decd2

--- src/applicake/applications/proteomics/openswath/featurexmltotsv.py
+++ src/applicake/applications/proteomics/openswath/featurexmltotsv.py
@@ -24,7 +24,7 @@ class FeatureXMLToTSV(IWrapper):
         """
         base = self.__class__.__name__
         self._template_file = '%s.tpl' % base # application specific config file
-        self._result_file = '%s.tsv' % base # result produced by the application
+        self._result_file = '%s.short.tsv' % base # result produced by the application
 
     def get_prefix(self,info,log):
         if not info.has_key(self.PREFIX):
