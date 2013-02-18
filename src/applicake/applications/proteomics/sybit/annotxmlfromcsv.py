17b0252ba4308c4e3e5e8db1a33597e7f9b58d90
blum@imsb.biol.ethz.ch <blum@imsb.biol.ethz.ch@a697f730-0300-b9c8-cee0-f846692decd2>
2013-02-18 15:31:31 +0000

    lfqpart2
    git-svn-id: http://applicake.googlecode.com/svn/trunk@1262 a697f730-0300-b9c8-cee0-f846692decd2

--- src/applicake/applications/proteomics/sybit/annotxmlfromcsv.py
+++ src/applicake/applications/proteomics/sybit/annotxmlfromcsv.py
@@ -46,61 +46,61 @@ class AnnotProtxmlFromCsv(IApplication):
             header = csv.reader([line]).next()
             for entry in csv.DictReader(source, header):
                 abundances = {}
-                for n in range(n_samples):
+                for n in range(1,n_samples):
                     abundances[sample_ids[n]] = entry["abundance_%d" % n]   
                 proteins = entry['protein'].split("/")
                 for protein in proteins:
                     prot_abundances[protein] = abundances                 
         return prot_abundances
     
-    def _annotate_protxml(self,xml_in,xml_out,prot_abundances):
-        from bs4 import BeautifulSoup
-        soup = BeautifulSoup(open(xml_in),"xml")
-        for protein in soup('protein'):
-            for abundance in protein(type='abundance'):
-                prot = str(protein['protein_name'])
-                id = str(abundance['name'])
-                lfquant = prot_abundances.get(prot, {}).get(id,'0')
-                if lfquant == '0':
-                    abundance.extract()
-                else:
-                    abundance['value'] = lfquant
-        
-        with open(xml_out,'w') as f:
-            f.write(soup.prettify())
+#    def _annotate_protxml(self,xml_in,xml_out,prot_abundances):
+#        from bs4 import BeautifulSoup
+#        soup = BeautifulSoup(open(xml_in),"xml")
+#        for protein in soup('protein'):
+#            for abundance in protein(type='abundance'):
+#                prot = str(protein['protein_name'])
+#                id = str(abundance['name'])
+#                lfquant = prot_abundances.get(prot, {}).get(id,'0')
+#                if lfquant == '0':
+#                    abundance.extract()
+#                else:
+#                    abundance['value'] = lfquant
+#        
+#        with open(xml_out,'w') as f:
+#            f.write(soup.prettify())
             
-#    def _annotate_protxml(self,xml_in,xml_out,prot_abundances,indent):
-#        #this code is copied from hendriks annotate_protxml(protein_path, protxml_input, export_path)
-#        # annotate protXML file:
-#        with open(xml_in) as source:
-#            with open(xml_out, "w") as sink:
-#                for line in source:
-#                    if line.strip().startswith("<protein " ):
-#                        protein = self._get_attribute_value(line, "protein_name")
-#                        base_indent = line[:line.index("<")]
-#                        if len(indent) <= len(base_indent):
-#                            indent = base_indent + indent
-#                    elif line.strip() == ("</protein>"): # insert abundances here
-#                        abundances = prot_abundances.get(protein, {})
-#                        for file_id, abundance in abundances.items():
-#                            if abundance != "0":
-#                                sink.write('%s<parameter name="%s" value="%s" '
-#                                           'type="abundance"/>\n' % 
-#                                           (indent, file_id, abundance))
-#                    elif line.strip().startswith("<parameter"):
-#                        try:
-#                            if self._get_attribute_value(line, "type") == "abundance":
-#                                continue # throw away old abundance entries
-#                        except ValueError: # different "parameter" element
-#                            pass
-#                    sink.write(line)
-#                    
-#    def _get_attribute_value(self,line, attribute):
-#        """Get the value of an attribute from the XML element contained in 'line'"""
-#        pos0 = line.index(attribute + '="')
-#        pos1 = line.index('"', pos0) + 1
-#        pos2 = line.index('"', pos1)
-#        return line[pos1:pos2]   
+    def _annotate_protxml(self,xml_in,xml_out,prot_abundances,indent):
+        #this code is copied from hendriks annotate_protxml(protein_path, protxml_input, export_path)
+        # annotate protXML file:
+        with open(xml_in) as source:
+            with open(xml_out, "w") as sink:
+                for line in source:
+                    if line.strip().startswith("<protein " ):
+                        protein = self._get_attribute_value(line, "protein_name")
+                        base_indent = line[:line.index("<")]
+                        if len(indent) <= len(base_indent):
+                            indent = base_indent + indent
+                    elif line.strip() == ("</protein>"): # insert abundances here
+                        abundances = prot_abundances.get(protein, {})
+                        for file_id, abundance in abundances.items():
+                            if abundance != "0":
+                                sink.write('%s<parameter name="%s" value="%s" '
+                                           'type="abundance"/>\n' % 
+                                           (indent, file_id, abundance))
+                    elif line.strip().startswith("<parameter"):
+                        try:
+                            if self._get_attribute_value(line, "type") == "abundance":
+                                continue # throw away old abundance entries
+                        except ValueError: # different "parameter" element
+                            pass
+                    sink.write(line)
+                    
+    def _get_attribute_value(self,line, attribute):
+        """Get the value of an attribute from the XML element contained in 'line'"""
+        pos0 = line.index(attribute + '="')
+        pos1 = line.index('"', pos0) + 1
+        pos2 = line.index('"', pos1)
+        return line[pos1:pos2]   
 
     def set_args(self,log,args_handler):
         args_handler.add_app_args(log, self.WORKDIR , 'Current WD')
