9b8639ffdd70f27f419104d0579bcd51c3062a14
blum@imsb.biol.ethz.ch <blum@imsb.biol.ethz.ch@a697f730-0300-b9c8-cee0-f846692decd2>
2013-03-26 15:43:57 +0000

    sequest dropbox
    git-svn-id: http://applicake.googlecode.com/svn/trunk@1350 a697f730-0300-b9c8-cee0-f846692decd2

--- executables/guse/run_copy2sequestdropbox.py
+++ executables/guse/run_copy2sequestdropbox.py
@@ -10,7 +10,7 @@ from applicake.framework.runner import IniFileRunner
 from applicake.applications.proteomics.sequest.dropbox import Copy2SequestDropbox
 
 runner = IniFileRunner()
-application = Copy2IdentDropbox()
+application = Copy2SequestDropbox()
 exit_code = runner(sys.argv,application)
 print exit_code
 sys.exit(exit_code)
