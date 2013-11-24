38153f6c00391277ce1f67700f3fd8c813bc543e
blum@imsb.biol.ethz.ch <blum@imsb.biol.ethz.ch@a697f730-0300-b9c8-cee0-f846692decd2>
2013-11-24 11:08:49 +0000

    openswathworkflow: cp to
    git-svn-id: http://applicake.googlecode.com/svn/trunk@1515 a697f730-0300-b9c8-cee0-f846692decd2

--- src/applicake/applications/proteomics/openswath/openswathworkflow.py
+++ src/applicake/applications/proteomics/openswath/openswathworkflow.py
@@ -10,51 +10,59 @@ MEM: can be controlled with -batchSize. 4000-5000 batchSize ~ 1G RAM per thread
 import os
 from applicake.framework.keys import Keys
 from applicake.framework.interfaces import IWrapper
-from applicake.framework.templatehandler import BasicTemplateHandler
 from applicake.utils.fileutils import FileUtils
-from applicake.utils.xmlutils import XmlValidator
 
 
 class OpenSwathWorkflow(IWrapper):
-
     def prepare_run(self, info, log):
         #in case getdataset instead of getmsdata was used key MZXML is not set but mzXML.gz is in DSSOUTPUT list
         if not Keys.MZXML in info:
-            if not isinstance(info[Keys.DSSOUTPUT],list):
+            if not isinstance(info[Keys.DSSOUTPUT], list):
                 info[Keys.DSSOUTPUT] = [info[Keys.DSSOUTPUT]]
             for key in info[Keys.DSSOUTPUT]:
                 if '.mzXML' in key:
                     info[Keys.MZXML] = key
 
-        tmpdir = os.environ.get('TMPDIR',info[Keys.WORKDIR]) + '/'
-        samplename = os.path.basename(info['MZXML']).split(".")[0]
-        info['FEATURETSV'] = os.path.join(info[Keys.WORKDIR],samplename + '.tsv')
-
-        #check for skip
-        chromml = ""
-        if info.get('DO_CHROMML_REQUANT',"") == "false":
-            log.info("Skipping creation of chromMZML")
-        else:
-            info['CHROM_MZML'] = os.path.join(info[Keys.WORKDIR],samplename + '.chrom.mzML')
-            chromml = "-out_chrom " + info['CHROM_MZML']
+        library = info['TRAML_CSV']
+        if info['TRAML_CSV'] == "":
+            log.warn("No tsv library found, using traml library. Affects mem usage significantly!")
+            library = info['TRAML']
 
         ppm = ''
         if info['WINDOW_UNIT'] == 'ppm':
             ppm = '-ppm'
 
-        library = info['TRAML_CSV']
-        if info['TRAML_CSV'] == "":
-            log.warn("No traml tsv found, using larger traml. affects mem usage significantly!")
-            library = info['TRAML']
+        samplename = os.path.basename(info['MZXML']).split(".")[0]
+        info['FEATURETSV'] = os.path.join(info[Keys.WORKDIR], samplename + '.tsv')
 
-        command = """OpenSwathWorkflow -in %s -tr %s -tr_irt %s -out_tsv %s %s
+        #copy mzxml, featureTSV and chormML first to local scratch, then back to global to decrease netI/O
+        tmpdir = os.environ.get('TMPDIR', info[Keys.WORKDIR]) + '/'
+        tmpmzxml = os.path.join(tmpdir, os.path.basename(info['MZXML']))
+        tmptsv = os.path.join(tmpdir, samplename + '.tsv.tmp')
+
+        if info.get('DO_CHROMML_REQUANT', "") == "false":
+            log.info("Skipping creation of chromMZML")
+            chrommlflag = ""
+            chrommlmv = "/bin/true"
+        else:
+            info['CHROM_MZML'] = os.path.join(info[Keys.WORKDIR], samplename + '.chrom.mzML')
+            tmpchromml = os.path.join(tmpdir, samplename + '.chrom.mzML.tmp')
+            chrommlflag = "-out_chrom " + tmpchromml
+            chrommlmv = " mv -v " + tmpchromml + " " + info['CHROM_MZML']
+
+        command = """cp -v %s %s &&
+        OpenSwathWorkflow -in %s -tr %s -tr_irt %s -out_tsv %s %s
         -min_rsq %s -min_coverage %s
         -min_upper_edge_dist %s -mz_extraction_window %s %s -rt_extraction_window %s
-        -tempDirectory %s -readOptions %s  -threads %s -batchSize %s""" % \
-        (info["MZXML"],library,info['IRTTRAML'], info['FEATURETSV'], chromml,
-         info['MIN_RSQ'],info['MIN_COVERAGE'],
-         info['MIN_UPPER_EDGE_DIST'], info['EXTRACTION_WINDOW'], ppm, info['RT_EXTRACTION_WINDOW'],
-         tmpdir,info["READ_OPTS"],info['THREADS'],info['BATCH_SIZE'])
+        -tempDirectory %s -readOptions %s  -threads %s -batchSize %s
+        && mv -v %s %s && %s""" % \
+                  (info["MZXML"], tmpmzxml,
+                   tmpmzxml, library, info['IRTTRAML'], tmptsv, chrommlflag,
+                   info['MIN_RSQ'], info['MIN_COVERAGE'],
+                   info['MIN_UPPER_EDGE_DIST'], info['EXTRACTION_WINDOW'], ppm, info['RT_EXTRACTION_WINDOW'],
+                   tmpdir, info["READ_OPTS"], info['THREADS'], info['BATCH_SIZE'],
+                   tmptsv, info['FEATURETSV'], chrommlmv
+                  )
 
         return command, info
 
@@ -90,11 +98,11 @@ class OpenSwathWorkflow(IWrapper):
         if 0 != run_code:
             return run_code, info
 
-        if not FileUtils.is_valid_file(log, info['FEATURETSV'] ):
-            log.critical('[%s] is not valid' % info['FEATURETSV'] )
-            return 1,info
+        if not FileUtils.is_valid_file(log, info['FEATURETSV']):
+            log.critical('[%s] is not valid' % info['FEATURETSV'])
+            return 1, info
 
-        if 'CHROM_MZML' in info and not FileUtils.is_valid_file(log,info['CHROM_MZML']):
-            log.critical('[%s] is not valid' % info['CHROM_MZML'] )
-            return 1,info
+        if 'CHROM_MZML' in info and not FileUtils.is_valid_file(log, info['CHROM_MZML']):
+            log.critical('[%s] is not valid' % info['CHROM_MZML'])
+            return 1, info
         return 0, info
