ffd25db38c3ffb9c1ec3995bf5e9a69d2f9c4f2e
blum@id.ethz.ch <blum@id.ethz.ch@a697f730-0300-b9c8-cee0-f846692decd2>
2014-05-21 09:27:39 +0000

    mergetoline rtforward as stubs
    git-svn-id: http://applicake.googlecode.com/svn/trunk@1606 a697f730-0300-b9c8-cee0-f846692decd2

--- appliapps/flow/mergetoline.py
+++ appliapps/flow/mergetoline.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 import shutil
-if __name__ == "__main__":
-    #merges input.ini to one line (for guse conditional execution check) and copies input to output
-    merge = open('input.ini').read().replace('\n', '')
-    print merge
-    open('merge.ini','w').write(merge)
-    shutil.copy("input.ini","output.ini")
+from applicake.app import BasicApp
+
+
+class MergeToLine(BasicApp):
+    @classmethod
+    def main(cls):
+        #merges input.ini to one line (for guse conditional execution check) and copies input to output
+        merge = open('input.ini').read().replace('\n', '')
+        print merge
+        open('merge.ini','w').write(merge)
+        shutil.copy("input.ini","output.ini")
--- appliapps/libcreation/rtforward.py
+++ appliapps/libcreation/rtforward.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 import os
 import shutil
+from applicake.app import BasicApp
 
-if __name__ == "__main__":
-    print os.listdir(".")
 
-    if "RUNRT = True" in open("uncalib.ini_0").read():
-        print "RUNRT WAS SET TRUE. TRYING TO CONTINUE USING RT-CALIBRATED INPUT"
-        shutil.copy("rtcalib.ini_0", "output.ini")
-    else:
-        print "RUNRT WAS SET FALSE. TRYING TO CONTINUE USING NON-CALIBRATED INPUT"
-        shutil.copy("uncalib.ini_0", "output.ini")
+class RTforward(BasicApp):
+    @classmethod
+    def main(cls):
+        print os.listdir(".")
+
+        if "RUNRT = True" in open("uncalib.ini_0").read():
+            print "RUNRT WAS SET TRUE. TRYING TO CONTINUE USING RT-CALIBRATED INPUT"
+            shutil.copy("rtcalib.ini_0", "output.ini")
+        else:
+            print "RUNRT WAS SET FALSE. TRYING TO CONTINUE USING NON-CALIBRATED INPUT"
+            shutil.copy("uncalib.ini_0", "output.ini")
