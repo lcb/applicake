94b78c402ecbb62ba647efeac49568f483da74c1
loblum <blum@id.ethz.ch>
2014-10-12 18:23:55 +0200

    extendWFID

--- executables/var/reportSWATH.R
+++ executables/var/reportSWATH.R
@@ -1,3 +1,3 @@
 #! /usr/bin/env Rscript
-Sweave("/cluster/apps/guse/stable/bin/analyseSWATH.Rnw")
+Sweave("/cluster/apps/guse/stable/applicake/executables/var/analyseSWATH.Rnw")
 tools::texi2dvi("analyseSWATH.tex",pdf=TRUE)
