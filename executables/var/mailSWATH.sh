1df7c8d227126cef2725acbfa6ad4ac26ff3b208
loblum <blum@id.ethz.ch>
2014-11-24 16:30:20 +0100

    mailSWATH

--- executables/var/mailSWATH.sh
+++ executables/var/mailSWATH.sh
@@ -9,19 +9,18 @@ then
 	echo reportSWATH.R "$1"
 	reportSWATH.R "$1"
 	exitcode=$?
+
+	if [ "$exitcode" == "0" -a -e "analyseSWATH.pdf" ]
+    then
+	    echo OK
+	    echo "Your SWATH analysis [$2] and report on $1 finished and will show up in openBIS soon!" | mail -s "SWATH finished - Report" -a analyseSWATH.pdf $(whoami)@ethz.ch
+    else
+    	echo SOME ERROR
+    	echo "Your SWATH analysis [$2] finished and will show up in openBIS soon! Unfortunately the report on $1 failed, so there is no PDF report" | mail -s "SWATH finished - no Report" $(whoami)@ethz.ch,loblum@ethz.ch
+    fi
 else
-	echo reportSWATHsingle.R "$1"
-	false
-	exitcode=$?
+    echo "Your SWATH analysis [$2] finished and will show up in openBIS soon! Unfortunately the report requires at least 2 samples, so there is no PDF report" | mail -s "SWATH finished - no Report" $(whoami)@ethz.ch
 fi
 
-exit
 
-if [ "$exitcode" == "0" -a -e "analyseSWATH.pdf" ]
-then
-	echo OK
-	echo "Your SWATH analysis [$2] and report on $1 finished and will show up in openBIS soon!" | mail -s "SWATH finished - Report" -a analyseSWATH.pdf $(whoami)@ethz.ch
-else
-	echo SOME ERROR
-	echo "Your SWATH analysis [$2] finished and will show up in openBIS soon! Unfortunately the report on $1 failed, so there is no PDF report" | mail -s "SWATH finished - no Report" $(whoami)@ethz.ch,loblum@ethz.ch
-fi
+
