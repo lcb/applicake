8c7e771edac10381e5e516e7cfa2c7928ada7af8
loblum <blum@id.ethz.ch>
2015-09-02 11:12:06 +0200

    mayu 1.08

--- appliapps/openbis/processexperiment.py
+++ appliapps/openbis/processexperiment.py
@@ -2,6 +2,7 @@
 from applicake.app import BasicApp
 from applicake.coreutils.arguments import Argument
 from applicake.coreutils.keys import Keys
+import re
 
 
 class ProcessExperiment(BasicApp):
@@ -18,7 +19,7 @@ class ProcessExperiment(BasicApp):
                 info[Keys.PROTXML] = entry
             if entry.endswith('pep.xml'):
                 info[Keys.PEPXML] = entry
-            if entry.endswith('_main_1.08.csv'):
+            if re.match(".*_main_1.0..csv",entry):
                 info['MAYUOUT'] = entry
             if entry.endswith('.properties'):
                 propfile = entry
