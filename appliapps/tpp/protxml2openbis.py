801edc499f2e3913372c888ff919fc2494a126ae
loblum <blum@id.ethz.ch>
2016-01-14 10:56:43 +0100

    fixed protxml2openbis

--- appliapps/tpp/protxml2openbis.py
+++ appliapps/tpp/protxml2openbis.py
@@ -32,12 +32,12 @@ class ProtXml2OpenbisSequence(WrappedApp):
         bisprotxml = os.path.join(wd, 'protxml2openbis.prot.xml')
 
         command = """pepxml2csv -IPROPHET -header -cP=%s -OUT=%s %s &&
-        protxml2spectralcount -CSV=%s -OUT=%s %s &&
-        protxml2modifications.py -TYP %s -CSV=%s -OUT=%s %s &&
+        protxml2spectralcount.py -CSV=%s -TYP %s -OUT=%s %s &&
+        protxml2modifications -CSV=%s -OUT=%s %s &&
         protxml2openbis -DB=%s -OUT=%s %s""" % (
             info['IPROB'], info['PEPCSV'], info[Keys.PEPXML],
-            info['PEPCSV'], countprotxml, info['PROTXML'],
-            info['PEPCSV'], info['SPECTRALCOUNT_TYPE'], modprotxml, countprotxml,
+            info['PEPCSV'], info['SPECTRALCOUNT_TYPE'],  countprotxml, info['PROTXML'],
+            info['PEPCSV'], modprotxml, countprotxml,
             info['DBASE'], bisprotxml, modprotxml )
 
         info['PROTXML'] = bisprotxml
